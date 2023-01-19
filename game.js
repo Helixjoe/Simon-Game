6096837e22ca6abdda33fa19d008118c89d56d11
Joel John <joeljohn5112@gmail.com>
2023-01-19 15:42:39 +0530

    random color generator added

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("Thing work");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence];
+
+gamePattern.push(randomChosenColour);
