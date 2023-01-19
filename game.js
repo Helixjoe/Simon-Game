abdcfe1740d9b88d90a10e47068fcc687b0470bf
Joel John <joeljohn5112@gmail.com>
2023-01-19 16:24:05 +0530

    flash animation added

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-alert("Thing work");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+var randomChosenColour = "." + buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+var Button = document.querySelector(randomChosenColour);
+
+function ButtonAnimation(Button) {
+  Button.classList.add("pressed");
+  setTimeout(function () {
+    Button.classList.remove("pressed");
+  }, 3000);
+}
+
+ButtonAnimation(Button);
