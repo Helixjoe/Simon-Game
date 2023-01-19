fc50993cfcb1cf31590cea8fb7ee9827a170fb5f
Joel John <joeljohn5112@gmail.com>
2023-01-19 16:37:23 +0530

    Color Detector Added

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+// Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+
+// // 2. Inside the handler, create a new variable called userChosenColour to store the id
+// of the button that got clicked.
+
+var userClickedPattern = [];
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
@@ -20,3 +27,11 @@ function ButtonAnimation(Button) {
 }
 
 ButtonAnimation(Button);
+
+// Button Identifier
+
+for (var i = 0; i < buttonColours.length; i++)
+  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
+    var userChosenColour = this.classList[1];
+    userClickedPattern.push(userChosenColour);
+  });
