849122c83e277bc3486dfeeb4fa806cfd6d22553
Joel John <joeljohn5112@gmail.com>
2023-01-20 21:01:00 +0530

    Button Sounds added

--- game.js
+++ game.js
@@ -1,32 +1,27 @@
-// Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
-
-// // 2. Inside the handler, create a new variable called userChosenColour to store the id
-// of the button that got clicked.
-
 var userClickedPattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
-
-var randomChosenColour = "." + buttonColours[nextSequence()];
-
-gamePattern.push(randomChosenColour);
-
-var Button = document.querySelector(randomChosenColour);
 
 function ButtonAnimation(Button) {
   Button.classList.add("pressed");
   setTimeout(function () {
     Button.classList.remove("pressed");
-  }, 3000);
+  }, 100);
 }
-
-ButtonAnimation(Button);
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = "." + buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  var Button = document.querySelector(randomChosenColour);
+  ButtonAnimation(Button);
+  playSound(buttonColours[randomNumber]); //Audio
+  console.log(buttonColours[randomNumber]);
+}
 
 // Button Identifier
 
@@ -34,4 +29,7 @@ for (var i = 0; i < buttonColours.length; i++)
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     var userChosenColour = this.classList[1];
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    var currentButton = document.querySelector("." + userChosenColour);
+    ButtonAnimation(currentButton);
   });
