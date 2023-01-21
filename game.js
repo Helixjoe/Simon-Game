0e7096e9d3f96e53259e88763ac7e4c324334eb9
Joel John <joeljohn5112@gmail.com>
2023-01-21 17:32:11 +0530

    var keyword replaced with let

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-var userClickedPattern = [];
-var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-var level = 0;
+let userClickedPattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let currentlevel = 0;
+let userClickIndex = 0;
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  let audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
@@ -15,33 +16,47 @@ function ButtonAnimation(Button) {
   }, 100);
 }
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = "." + buttonColours[randomNumber];
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  var Button = document.querySelector(randomChosenColour);
+  let Button = document.querySelector("." + randomChosenColour);
   ButtonAnimation(Button);
-  playSound(buttonColours[randomNumber]); //Audio
-  level++;
-  document.querySelector("#level-title").innerHTML = "Level " + level;
+  playSound(randomChosenColour); //Audio
+  document.querySelector("#level-title").innerHTML = "Level " + currentlevel;
 }
 
-// Button Identifier
-
-for (var i = 0; i < buttonColours.length; i++)
-  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
-    var userChosenColour = this.classList[1];
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    var currentButton = document.querySelector("." + userChosenColour);
-    ButtonAnimation(currentButton);
-  });
+function checkAnswer(currentLevel, index) {
+  if (userClickedPattern[index] === gamePattern[index]) {
+    console.log("right");
+    currentLevel++;
+    setTimeout(nextSequence, 1000);
+    userClickedPattern = [];
+  } else {
+    console.log("wrong");
+  }
+}
 
 //Start the Game
 
 document.addEventListener("keydown", function (event) {
-  var Key = event.key;
+  let Key = event.key;
   if (Key === "a") {
-    document.querySelector("#level-title").innerHTML = "Level " + level;
+    document.querySelector("#level-title").innerHTML = "Level " + currentlevel;
     nextSequence();
   }
 });
+
+// Button Identifier
+
+for (let i = 0; i < buttonColours.length; i++)
+  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
+    let userChosenColour = this.classList[1];
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    userClickIndex++;
+    if (userClickIndex === gamePattern.length) {
+      checkAnswer(currentlevel, userClickIndex);
+    }
+    let currentButton = document.querySelector("." + userChosenColour);
+    ButtonAnimation(currentButton);
+  });
