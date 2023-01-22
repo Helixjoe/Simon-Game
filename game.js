eea2cb27d1a181d91dcc90619a2e5635b83fa35b
Joel John <joeljohn5112@gmail.com>
2023-01-22 17:10:24 +0530

    fixed the flow of game

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let userClickedPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let currentlevel = 0;
-let userClickIndex = 0;
+let userClickIndex = -1;
 
 function playSound(name) {
   let audio = new Audio("sounds/" + name + ".mp3");
@@ -29,8 +29,9 @@ function checkAnswer(currentLevel, index) {
   if (userClickedPattern[index] === gamePattern[index]) {
     console.log("right");
     currentLevel++;
-    setTimeout(nextSequence, 1000);
+    userClickIndex = -1;
     userClickedPattern = [];
+    setTimeout(nextSequence, 1000);
   } else {
     console.log("wrong");
   }
@@ -42,6 +43,7 @@ document.addEventListener("keydown", function (event) {
   let Key = event.key;
   if (Key === "a") {
     document.querySelector("#level-title").innerHTML = "Level " + currentlevel;
+    userClickedPattern = [];
     nextSequence();
   }
 });
@@ -54,9 +56,9 @@ for (let i = 0; i < buttonColours.length; i++)
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     userClickIndex++;
-    if (userClickIndex === gamePattern.length) {
-      checkAnswer(currentlevel, userClickIndex);
-    }
     let currentButton = document.querySelector("." + userChosenColour);
     ButtonAnimation(currentButton);
+    if (userClickIndex === gamePattern.length - 1) {
+      checkAnswer(currentlevel, userClickIndex);
+    }
   });
