64a41ac9cb1ee02f63993c0060631e4439823525
Joel John <joeljohn5112@gmail.com>
2023-01-22 17:21:35 +0530

    Level displayer fixed

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let userClickedPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-let currentlevel = 0;
+let currentLevel = 0;
 let userClickIndex = -1;
 
 function playSound(name) {
@@ -22,15 +22,15 @@ function nextSequence() {
   let Button = document.querySelector("." + randomChosenColour);
   ButtonAnimation(Button);
   playSound(randomChosenColour); //Audio
-  document.querySelector("#level-title").innerHTML = "Level " + currentlevel;
+  document.querySelector("#level-title").innerHTML = "Level " + currentLevel;
 }
 
 function checkAnswer(currentLevel, index) {
   if (userClickedPattern[index] === gamePattern[index]) {
     console.log("right");
-    currentLevel++;
     userClickIndex = -1;
     userClickedPattern = [];
+    document.querySelector("#level-title").innerHTML = "Level " + currentLevel;
     setTimeout(nextSequence, 1000);
   } else {
     console.log("wrong");
@@ -42,7 +42,7 @@ function checkAnswer(currentLevel, index) {
 document.addEventListener("keydown", function (event) {
   let Key = event.key;
   if (Key === "a") {
-    document.querySelector("#level-title").innerHTML = "Level " + currentlevel;
+    document.querySelector("#level-title").innerHTML = "Level " + currentLevel;
     userClickedPattern = [];
     nextSequence();
   }
@@ -59,6 +59,7 @@ for (let i = 0; i < buttonColours.length; i++)
     let currentButton = document.querySelector("." + userChosenColour);
     ButtonAnimation(currentButton);
     if (userClickIndex === gamePattern.length - 1) {
-      checkAnswer(currentlevel, userClickIndex);
+      checkAnswer(currentLevel, userClickIndex);
+      currentLevel++;
     }
   });
